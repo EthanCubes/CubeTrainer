04b108f7f0a340d60c24964078b15509881c7fd7
EthanCubes <ethanshangyilin@protonmail.com>
2026-06-22 12:28:54 -0600

    Added button to be able to start and stop timer.

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 let startTime;
 let stopTime;
+let timing = false;
 
 function startTimer() {
     startTime = Date.now();
+    timing = true;
 }
 
 function stopTimer() {
     stopTime = Date.now();
+    timing = false;
     console.log(stopTime - startTime);
+}
+
+function timerTrigger() {
+    if (timing) {
+        stopTimer();
+    } 
+    else {
+        startTimer();
+    }
 }
