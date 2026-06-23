0ca412095dc2a2807780c19411212c64f15d5024
EthanCubes <ethanshangyilin@protonmail.com>
2026-06-22 19:45:50 -0600

    "Trigger" button now changes colour and contents according to if the timer is timing.

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ setInterval(updateTimer, 10);
 function startTimer() {
     startTime = Date.now();
     timing = true;
+
+    const trigger = document.getElementById("trigger");
+    trigger.innerHTML = "Stop";
+    trigger.style.color = "red";
 }
 
 function stopTimer() {
@@ -15,6 +19,9 @@ function stopTimer() {
     timing = false;
     time = stopTime - startTime;
     console.log(time);
+    const trigger = document.getElementById("trigger");
+    trigger.innerHTML = "Start";
+    trigger.style.color = "black";
 }
 
 function timerTrigger() {
