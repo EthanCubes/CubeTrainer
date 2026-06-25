8fa3a37f5ed639766817788f540922e9d7844445
EthanCubes <ethanshangyilin@protonmail.com>
2026-06-25 15:58:14 -0600

    Created script to render cube net according to position of cube in the array of "drawingCalculator.js".

---
+++ src/drawingRender.js
@@ -0,0 +1,38 @@
+function changeSticker(element, color) {
+    if (color === 1) {
+        element.style.backgroundColor = "white";
+    }
+    else {
+        if (color === 2) {
+            element.style.backgroundColor = "orange";
+        }
+        else {
+            if (color === 3) {
+                element.style.backgroundColor = "green";
+            }
+            else {
+                if (color === 4) {
+                    element.style.backgroundColor = "red";
+                }
+                else {
+                    if (color === 5) {
+                        element.style.backgroundColor = "blue";
+                    }
+                    else {
+                        element.style.backgroundColor = "yellow";
+                    }
+                }
+            }
+        }
+    }
+}
+
+function renderDrawing() {
+    for (let i = 0; i < 54; i++) {
+        console.log(i);
+        let sticker = document.getElementById("grid" + (i));
+        changeSticker(sticker, cubePos[i]);
+    }
+}
+
+renderDrawing();
