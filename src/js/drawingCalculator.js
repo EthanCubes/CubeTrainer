30633b0220a38ba881b304a3a1984a907b842297
EthanCubes <ethanshangyilin@protonmail.com>
2026-06-25 17:59:44 -0600

    Added code for R move of cube graph.

--- src/js/drawingCalculator.js
+++ src/js/drawingCalculator.js
@@ -24,6 +24,7 @@ let cubePos = [1, 1, 1, 1, 1, 1, 1, 1, 1,
 
 function reset() {
     cubePos = JSON.parse(JSON.stringify(cubePosDefault));
+    renderDrawing();
 }
 
 function u() {
@@ -89,6 +90,7 @@ function u() {
 
     cubePos = JSON.parse(JSON.stringify(cubePosTemp));
     console.log(cubePos);
+    renderDrawing();
 }
 
 function uPrime() {
@@ -154,4 +156,71 @@ function uPrime() {
 
     cubePos = JSON.parse(JSON.stringify(cubePosTemp));
     console.log(cubePos);
+    renderDrawing();
+}
+
+function r() {
+    // white
+    cubePosTemp[0] = cubePos[0];
+    cubePosTemp[1] = cubePos[1];
+    cubePosTemp[2] = cubePos[20];
+    cubePosTemp[3] = cubePos[3];
+    cubePosTemp[4] = cubePos[4];
+    cubePosTemp[5] = cubePos[23];
+    cubePosTemp[6] = cubePos[6];
+    cubePosTemp[7] = cubePos[7];
+    cubePosTemp[8] = cubePos[26];
+
+    // orange
+    for (let i = 0; i < 9; i++) {
+        cubePosTemp[i + 9] = cubePos[i + 9];
+    }
+
+    // green
+    cubePosTemp[18] = cubePos[18];
+    cubePosTemp[19] = cubePos[19];
+    cubePosTemp[20] = cubePos[47];
+    cubePosTemp[21] = cubePos[21];
+    cubePosTemp[22] = cubePos[22];
+    cubePosTemp[23] = cubePos[50];
+    cubePosTemp[24] = cubePos[24];
+    cubePosTemp[25] = cubePos[25];
+    cubePosTemp[26] = cubePos[53];
+
+    // red
+    cubePosTemp[27] = cubePos[33];
+    cubePosTemp[28] = cubePos[30];
+    cubePosTemp[29] = cubePos[27];
+    cubePosTemp[30] = cubePos[34];
+    cubePosTemp[31] = cubePos[31];
+    cubePosTemp[32] = cubePos[28];
+    cubePosTemp[33] = cubePos[35];
+    cubePosTemp[34] = cubePos[32];
+    cubePosTemp[35] = cubePos[29];
+
+    // blue
+    cubePosTemp[36] = cubePos[8];
+    cubePosTemp[37] = cubePos[37];
+    cubePosTemp[38] = cubePos[38];
+    cubePosTemp[39] = cubePos[5];
+    cubePosTemp[40] = cubePos[40];
+    cubePosTemp[41] = cubePos[41];
+    cubePosTemp[42] = cubePos[2];
+    cubePosTemp[43] = cubePos[43];
+    cubePosTemp[44] = cubePos[44];
+
+    // yellow
+    cubePosTemp[45] = cubePos[45];
+    cubePosTemp[46] = cubePos[46];
+    cubePosTemp[47] = cubePos[42];
+    cubePosTemp[48] = cubePos[48];
+    cubePosTemp[49] = cubePos[49];
+    cubePosTemp[50] = cubePos[39];
+    cubePosTemp[51] = cubePos[51];
+    cubePosTemp[51] = cubePos[52];
+    cubePosTemp[53] = cubePos[36];
+
+    cubePos = JSON.parse(JSON.stringify(cubePosTemp));
+    console.log(cubePos);
+    renderDrawing();
 }
