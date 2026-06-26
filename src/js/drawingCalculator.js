2e2ff70e82934ccfbf4afb0cb2b3e988280f978d
EthanCubes <ethanshangyilin@protonmail.com>
2026-06-25 21:54:23 -0600

    Created X move (cube rotation to replace basic moves)

--- src/js/drawingCalculator.js
+++ src/js/drawingCalculator.js
@@ -1,4 +1,4 @@
-// dumb ahh code
+// dumb ahh code (all handwritten btw)
 
 const cubePosDefault = [1, 1, 1, 1, 1, 1, 1, 1, 1,
     2, 2, 2, 2, 2, 2, 2, 2, 2,
@@ -159,164 +159,38 @@ function uPrime() {
     renderDrawing();
 }
 
-function fMove() {
+function xMove() {
     // white
-    cubePosTemp[0] = cubePos[0];
-    cubePosTemp[1] = cubePos[1];
-    cubePosTemp[2] = cubePos[2];
-    cubePosTemp[3] = cubePos[3];
-    cubePosTemp[4] = cubePos[4];
-    cubePosTemp[5] = cubePos[5];
-    cubePosTemp[6] = cubePos[17];
-    cubePosTemp[7] = cubePos[14];
-    cubePosTemp[8] = cubePos[11];
-
-    // orange
-    cubePosTemp[9] = cubePos[9];
-    cubePosTemp[10] = cubePos[10];
-    cubePosTemp[11] = cubePos[45];
-    cubePosTemp[12] = cubePos[12];
-    cubePosTemp[13] = cubePos[13];
-    cubePosTemp[14] = cubePos[46];
-    cubePosTemp[15] = cubePos[15];
-    cubePosTemp[16] = cubePos[16];
-    cubePosTemp[17] = cubePos[47];
-
-    // green
-    cubePosTemp[18] = cubePos[24];
-    cubePosTemp[19] = cubePos[21];
-    cubePosTemp[20] = cubePos[18];
-    cubePosTemp[21] = cubePOs[25];
-    cubePosTemp[22] = cubePos[22];
-    cubePosTemp[23] = cubePos[19];
-    cubePosTemp[24] = cubePos[26];
-    cubePosTemp[25] = cubePos[23];
-    cubePosTemp[26] = cubePos[20];
-
-    // red
-    cubePosTemp[27] = cubePos[6];
-    cubePosTemp[28] = cubePos[28];
-    cubePosTemp[29] = cubePos[29];
-    cubePosTemp[30] = cubePos[7];
-    cubePosTemp[31] = cubePos[31];
-    cubePosTemp[32] = cubePos[32];
-    cubePosTemp[33] = cubePos[8];
-    cubePosTemp[34] = cubePos[34];
-    cubePosTemp[35] = cubePos[35];
-
-    // blue
-    for (let i = 0; i < 9; i++) {
-        cubePosTemp[i + 36] = cubePos[i + 36];
-    }
-
-    // yellow
-    cubePosTemp[45] = cubePos[33];
-    cubePosTemp[46] = cubePos[30];
-    cubePosTemp[47] = cubePos[27];
-    cubePosTemp[48] = cubePos[48];
-    cubePosTemp[49] = cubePos[49];
-    cubePosTemp[50] = cubePos[50];
-    cubePosTemp[51] = cubePos[51];
-    cubePosTemp[52] = cubePos[52];
-    cubePosTemp[53] = cubePos[53];
-
-    cubePos = JSON.parse(JSON.stringify(cubePosTemp));
-    console.log(cubePos);
-    renderDrawing();
-}
-
-function fPrime() {
-    // white
-    cubePosTemp[0] = cubePos[0];
-    cubePosTemp[1] = cubePos[1];
-    cubePosTemp[2] = cubePos[2];
-    cubePosTemp[3] = cubePos[3];
-    cubePosTemp[4] = cubePos[4];
-    cubePosTemp[5] = cubePos[5];
-    cubePosTemp[6] = cubePos[27];
-    cubePosTemp[7] = cubePos[30];
-    cubePosTemp[8] = cubePos[33];
-
-    // orange
-    cubePosTemp[9] = cubePos[9];
-    cubePosTemp[10] = cubePos[10];
-    cubePosTemp[11] = cubePos[8];
-    cubePosTemp[12] = cubePos[12];
-    cubePosTemp[13] = cubePos[13];
-    cubePosTemp[14] = cubePos[7];
-    cubePosTemp[15] = cubePos[15];
-    cubePosTemp[16] = cubePos[16];
-    cubePosTemp[17] = cubePos[6];
-
-    // green
-    cubePosTemp[18] = cubePos[20];
-    cubePosTemp[19] = cubePos[23];
-    cubePosTemp[20] = cubePos[26];
-    cubePosTemp[21] = cubePos[19];
-    cubePosTemp[22] = cubePos[22];
-    cubePosTemp[23] = cubePos[25];
-    cubePosTemp[24] = cubePos[18];
-    cubePosTemp[25] = cubePos[21];
-    cubePosTemp[26] = cubePos[22];
-
-    // red
-    cubePosTemp[27] = cubePos[45];
-    cubePosTemp[28] = cubePos[28];
-    cubePosTemp[29] = cubePos[29];
-    cubePosTemp[30] = cubePos[46];
-    cubePosTemp[31] = cubePos[31];
-    cubePosTemp[32] = cubePos[32];
-    cubePosTemp[33] = cubePos[47];
-    cubePosTemp[34] = cubePos[34];
-    cubePosTemp[35] = cubePos[35];
-
-    // blue
-    for (let i = 0; i < 9; i++) {
-        cubePosTemp[i + 36] = cubePos[i + 36];
-    }
-
-    // yellow
-    cubePosTemp[45] = cubePos[11];
-    cubePosTemp[46] = cubePos[14];
-    cubePosTemp[47] = cubePos[17];
-    cubePosTemp[48] = cubePos[48];
-    cubePosTemp[49] = cubePos[49];
-    cubePosTemp[50] = cubePos[50];
-    cubePosTemp[51] = cubePos[51];
-    cubePosTemp[52] = cubePos[52];
-    cubePosTemp[53] = cubePos[53];
-
-    cubePos = JSON.parse(JSON.stringify(cubePosTemp));
-    console.log(cubePos);
-    renderDrawing();
-}
-
-function rMove() {
-    // white
-    cubePosTemp[0] = cubePos[0];
-    cubePosTemp[1] = cubePos[1];
+    cubePosTemp[0] = cubePos[18];
+    cubePosTemp[1] = cubePos[19];
     cubePosTemp[2] = cubePos[20];
-    cubePosTemp[3] = cubePos[3];
-    cubePosTemp[4] = cubePos[4];
+    cubePosTemp[3] = cubePos[21];
+    cubePosTemp[4] = cubePos[22];
     cubePosTemp[5] = cubePos[23];
-    cubePosTemp[6] = cubePos[6];
-    cubePosTemp[7] = cubePos[7];
+    cubePosTemp[6] = cubePos[24];
+    cubePosTemp[7] = cubePos[25];
     cubePosTemp[8] = cubePos[26];
 
     // orange
-    for (let i = 0; i < 9; i++) {
-        cubePosTemp[i + 9] = cubePos[i + 9];
-    }
+    cubePosTemp[9] = cubePos[11];
+    cubePosTemp[10] = cubePos[14];
+    cubePosTemp[11] = cubePos[17];
+    cubePosTemp[12] = cubePos[10];
+    cubePosTemp[13] = cubePos[13];
+    cubePosTemp[14] = cubePos[16];
+    cubePosTemp[15] = cubePos[9];
+    cubePosTemp[16] = cubePos[12];
+    cubePosTemp[17] = cubePos[15];
 
-    // green
-    cubePosTemp[18] = cubePos[18];
-    cubePosTemp[19] = cubePos[19];
+    // greem
+    cubePosTemp[18] = cubePos[45];
+    cubePosTemp[19] = cubePos[46];
     cubePosTemp[20] = cubePos[47];
-    cubePosTemp[21] = cubePos[21];
-    cubePosTemp[22] = cubePos[22];
+    cubePosTemp[21] = cubePos[48];
+    cubePosTemp[22] = cubePos[49];
     cubePosTemp[23] = cubePos[50];
-    cubePosTemp[24] = cubePos[24];
-    cubePosTemp[25] = cubePos[25];
+    cubePosTemp[24] = cubePos[51];
+    cubePosTemp[25] = cubePos[52];
     cubePosTemp[26] = cubePos[53];
 
     // red
@@ -332,91 +206,25 @@ function rMove() {
 
     // blue
     cubePosTemp[36] = cubePos[8];
-    cubePosTemp[37] = cubePos[37];
-    cubePosTemp[38] = cubePos[38];
+    cubePosTemp[37] = cubePos[7];
+    cubePosTemp[38] = cubePos[6];
     cubePosTemp[39] = cubePos[5];
-    cubePosTemp[40] = cubePos[40];
-    cubePosTemp[41] = cubePos[41];
+    cubePosTemp[40] = cubePos[4];
+    cubePosTemp[41] = cubePos[3];
     cubePosTemp[42] = cubePos[2];
-    cubePosTemp[43] = cubePos[43];
-    cubePosTemp[44] = cubePos[44];
+    cubePosTemp[43] = cubePos[1];
+    cubePosTemp[44] = cubePos[0];
 
     // yellow
-    cubePosTemp[45] = cubePos[45];
-    cubePosTemp[46] = cubePos[46];
+    cubePosTemp[45] = cubePos[44];
+    cubePosTemp[46] = cubePos[43];
     cubePosTemp[47] = cubePos[42];
-    cubePosTemp[48] = cubePos[48];
-    cubePosTemp[49] = cubePos[49];
+    cubePosTemp[48] = cubePos[41];
+    cubePosTemp[49] = cubePos[40];
     cubePosTemp[50] = cubePos[39];
-    cubePosTemp[51] = cubePos[51];
-    cubePosTemp[51] = cubePos[52];
+    cubePosTemp[51] = cubePos[38];
+    cubePosTemp[52] = cubePos[37];
     cubePosTemp[53] = cubePos[36];
-
-    cubePos = JSON.parse(JSON.stringify(cubePosTemp));
-    console.log(cubePos);
-    renderDrawing();
-}
-
-function rPrime() {
-    // white
-    cubePosTemp[0] = cubePos[0];
-    cubePosTemp[1] = cubePos[1];
-    cubePosTemp[2] = cubePos[42];
-    cubePosTemp[3] = cubePos[3];
-    cubePosTemp[4] = cubePos[4];
-    cubePosTemp[5] = cubePos[39];
-    cubePosTemp[6] = cubePos[6];
-    cubePosTemp[7] = cubePos[7];
-    cubePosTemp[8] = cubePos[36];
-
-    // orange
-    for (let i = 0; i < 9; i++) {
-        cubePosTemp[i + 9] = cubePos[i + 9];
-    }
-
-    // green
-    cubePosTemp[18] = cubePos[18];
-    cubePosTemp[19] = cubePos[19];
-    cubePosTemp[20] = cubePos[2];
-    cubePosTemp[21] = cubePos[21];
-    cubePosTemp[22] = cubePos[22];
-    cubePosTemp[23] = cubePos[5];
-    cubePosTemp[24] = cubePos[24];
-    cubePosTemp[25] = cubePos[25];
-    cubePosTemp[26] = cubePos[8];
-
-    // red
-    cubePosTemp[27] = cubePos[29];
-    cubePosTemp[28] = cubePos[32];
-    cubePosTemp[29] = cubePos[35];
-    cubePosTemp[30] = cubePos[28];
-    cubePosTemp[31] = cubePos[31];
-    cubePosTemp[32] = cubePos[34];
-    cubePosTemp[33] = cubePos[27];
-    cubePosTemp[34] = cubePos[30];
-    cubePosTemp[35] = cubePos[33];
-
-    // blue
-    cubePosTemp[36] = cubePos[53];
-    cubePosTemp[37] = cubePos[37];
-    cubePosTemp[38] = cubePos[38];
-    cubePosTemp[39] = cubePos[50];
-    cubePosTemp[40] = cubePos[40];
-    cubePosTemp[41] = cubePos[41];
-    cubePosTemp[42] = cubePos[47];
-    cubePosTemp[43] = cubePos[43];
-    cubePosTemp[44] = cubePos[44];
-
-    // yellow
-    cubePosTemp[45] = cubePos[45];
-    cubePosTemp[46] = cubePos[46];
-    cubePosTemp[47] = cubePos[20];
-    cubePosTemp[48] = cubePos[48];
-    cubePosTemp[49] = cubePos[49];
-    cubePosTemp[50] = cubePos[23];
-    cubePosTemp[51] = cubePos[51];
-    cubePosTemp[51] = cubePos[52];
-    cubePosTemp[53] = cubePos[26];
 
     cubePos = JSON.parse(JSON.stringify(cubePosTemp));
     console.log(cubePos);
