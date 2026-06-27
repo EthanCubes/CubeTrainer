e736fa8d29287247e2c03e2d13657bf0b8439057
EthanCubes <ethanshangyilin@protonmail.com>
2026-06-26 21:20:28 -0600

    Made dropdown menu (mode select) toggle another drop down menu (set select) so the set only appears on certian modes.

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,19 @@
-const dropDown = document.getElementById("dropDown");
+const modeSelect = document.getElementById("modeSelect");
 let mode;
 
-dropDown.addEventListener("change", function() {mode = dropDown.value; console.log(mode);});
+const setSelect = document.getElementById("setSelect");
+
+modeSelect.addEventListener("change", modeChanged);
+
+function modeChanged() {
+    mode = modeSelect.value;
+    console.log(mode);
+    switch (mode) {
+        case "timer":
+            setSelect.style.display = "none";
+            break;
+        case "driller":
+            setSelect.style.display = "inline-block";
+            break;
+    }
+}
