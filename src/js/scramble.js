5440884c56311106c420cdebf0690fe26ae2136d
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-07 20:32:56 -0600

    Added compatibility for double prime moves to flipScramble() function.

--- src/js/scramble.js
+++ src/js/scramble.js
@@ -42,7 +42,22 @@ function generateScramble() {
         case "driller":
             method = algorithmSet.method;
             stage = algorithmSet.stage;
-            if (method === "CFOP") {
+            if ((method === "roux") && (stage === "EO")) {
+                let scrambleObj = pickScramble();
+                scramble = scrambleObj.algorithm;
+                scrambleMoveList = scramble.split(/\s+/);
+                flipScramble();
+                let prefixKey = scrambleObj.endPos;
+                console.log(prefixKey);
+                let prefix = algorithmSet.endPosScramble[prefixKey];
+                scramble = prefix + " " + scramble;
+                scrambleMoveList = scramble.split(/\s+/);
+                console.log(scramble);
+                changeScrambleText();
+                changeDrawing();
+                break;
+            }
+            else {
                 let scrambleObj = pickScramble();
                 scramble = scrambleObj.algorithm;
                 scrambleMoveList = scramble.split(/\s+/);
@@ -51,35 +66,6 @@ function generateScramble() {
                 changeScrambleText();
                 changeDrawing();
                 break;
-            }
-            else {
-                if (method === "roux") {
-                    if (!(stage === "CMLL")) {
-                        let scrambleObj = pickScramble();
-                        scramble = scrambleObj.algorithm;
-                        scrambleMoveList = scramble.split(/\s+/);
-                        flipScramble();
-                        let prefixKey = scrambleObj.endPos;
-                        console.log(prefixKey);
-                        let prefix = algorithmSet.endPosScramble[prefixKey];
-                        scramble = prefix + " " + scramble;
-                        scrambleMoveList = scramble.split(/\s+/);
-                        console.log(scramble);
-                        changeScrambleText();
-                        changeDrawing();
-                        break;
-                    }
-                    else {
-                        let scrambleObj = pickScramble();
-                        scramble = scrambleObj.algorithm;
-                        scrambleMoveList = scramble.split(/\s+/);
-                        flipScramble();
-                        console.log(scramble);
-                        changeScrambleText();
-                        changeDrawing();
-                        break;
-                    }
-                }
             }
     }
 }
@@ -99,110 +85,137 @@ function flipScramble() {
     const flippedMoves = {
         "U": "U'",
         "U'": "U",
-        "U2": "U2",
+        "U2": "U2'",
+        "U2'": "U2",
 
         "L": "L'",
         "L'": "L",
         "L2": "L2",
+        "L2'": "L2",
 
         "F": "F'",
         "F'": "F",
         "F2": "F2",
+        "F2'": "F2",
 
         "R": "R'",
         "R'": "R",
         "R2": "R2",
+        "R2'": "R2",
 
         "B": "B'",
         "B'": "B",
         "B2": "B2",
+        "B2'": "B2",
 
         "D": "D'",
         "D'": "D",
         "D2": "D2",
+        "D2'": "D2",
 
 
         "u": "u'",
         "u'": "u",
         "u2": "u2",
+        "u2'": "u2",
 
         "l": "l'",
         "l'": "l",
         "l2": "l2",
+        "l2'": "l2",
 
         "f": "f'",
         "f'": "f",
         "f2": "f2",
+        "f2'": "f2",
 
         "r": "r'",
         "r'": "r",
         "r2": "r2",
+        "r2'": "r2",
 
         "b": "b'",
         "b'": "b",
         "b2": "b2",
+        "b2'": "b2",
 
         "d": "d'",
         "d'": "d",
         "d2": "d2",
+        "d2'": "d2",
 
         "Uw": "Uw'",
         "Uw'": "Uw",
         "Uw2": "Uw2",
+        "Uw2'": "Uw2",
 
         "Lw": "Lw'",
         "Lw'": "Lw",
         "Lw2": "Lw2",
+        "Lw2'": "Lw2",
 
         "Fw": "Fw'",
         "Fw'": "Fw",
         "Fw2": "Fw2",
+        "Fw2'": "Fw2",
 
         "Rw": "Rw'",
         "Rw'": "Rw",
         "Rw2": "Rw2",
+        "Rw2'": "Rw2",
 
         "Bw": "Bw'",
         "Bw'": "Bw",
         "Bw2": "Bw2",
+        "Bw2'": "Bw2",
 
         "Dw": "Dw'",
         "Dw'": "Dw",
         "Dw2": "Dw2",
+        "Dw2'": "Dw2",
 
 
         "M": "M'",
         "M'": "M",
         "M2": "M2",
+        "M2'": "M2",
 
         "E": "E'",
         "E'": "E",
         "E2": "E2",
+        "E2'": "E2",
 
         "S": "S'",
         "S'": "S",
         "S2": "S2",
+        "S2'": "S2",
 
 
         "x": "x'",
         "x'": "x",
         "x2": "x2",
+        "x2'": "x2",
 
         "y": "y'",
         "y'": "y",
         "y2": "y2",
+        "y2'": "y2",
 
         "z": "z'",
         "z'": "z",
         "z2": "z2",
+        "z2'": "z2"
     }
 
+    console.log(scrambleMoveList);
     for (let i = 0; i < scrambleMoveList.length; i++) {
         scrambleMoveList[i] = flippedMoves[scrambleMoveList[i]];
     }
+    console.log(scrambleMoveList);
     scrambleMoveList.reverse();
     for (let i = 0; i < scrambleMoveList.length; i++) {
-        flippedMoveScramble += scrambleMoveList[i] + " "
+        flippedMoveScramble += scrambleMoveList[i] + " ";
     }
     scramble = flippedMoveScramble;
+    console.log(scrambleMoveList);
 }
