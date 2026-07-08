21c38e2ee1d4683b4ac26a2bf1d3accedcfa8d8e
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-08 14:23:15 -0600

    Added settings in setting object regarding if certain elements are shown.

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,4 +1,9 @@
 let programSettings = {
+    "toggle": [
+        {"showDrawing": "on"},
+        {"showSolves": "on"},
+        {"showScramble": "on"}
+    ],
     "theme": [
         {"color1": "rgb(22, 96, 236)"},
         {"color2": "rgb(171, 218, 252)"},
