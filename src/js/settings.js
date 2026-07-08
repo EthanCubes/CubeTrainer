654097deb37f6fd1f7b3cc7996b131f163dbd15f
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-08 16:10:05 -0600

    Added function do update settings object when settings window is closed .

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,12 +1,23 @@
 let programSettings = {
     "toggle": [
-        {"showDrawing": "on"},
-        {"showSolves": "on"},
-        {"showScramble": "on"}
+        {"hideDrawing": false},
+        {"hideSolves": false},
+        {"hideScramble": false}
     ],
     "theme": [
         {"color1": "rgb(22, 96, 236)"},
         {"color2": "rgb(171, 218, 252)"},
         {"textColor": "rgb(0, 0, 0)"}
     ]
+}
+
+const hideDrawing = document.getElementById("hideDrawing");
+const hideSolves = document.getElementById("hideSolves");
+const hideScramble = document.getElementById("hideScramble");
+
+function updateSettings() {
+    // I tried using .value for so long until I gave up and used DeepSeek to search up what the correct thing was. Sorry.
+    programSettings.toggle[0] = hideDrawing.checked;
+    programSettings.toggle[1] = hideSolves.checked;
+    programSettings.toggle[2] = hideScramble.checked;
 }
